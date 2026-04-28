556d18a5549b81cfd837ebd517aa2605803546d3
Echo8485 <steve1921@icloud.com>
2026-04-28 23:29:20 +0900

    Loginpage

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import React from 'react';
 import Header from "./components/Header.js"
 import Footer from "./components/Footer.js"
 import "./App.css"
-import {Routes, Route}from "react-router-dom";
+import {Routes, Route,useLocation}from "react-router-dom";
 import MainPage from "./pages/Mainpage/MainPage.js";
+import LoginPage from "./pages/LoginPage/Loginpage.js";
 
 function App() {
+  const location = useLocation();
+  const isLoginPage = location.pathname === "/login";
   return (
     <div>
-      <Header/>
+      {!isLoginPage&& <Header/>}
       <Routes>
         <Route path="/" element={<MainPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
       </Routes>
-      <Footer/>
+      {!isLoginPage&& <Footer/>}
+      
     </div>
   );
 }
